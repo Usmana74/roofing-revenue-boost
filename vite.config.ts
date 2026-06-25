1944b764b919181f1581d613845b6a975a35c086
Usman Ahmad <176893780+Usmana74@users.noreply.github.com>
2026-06-25 09:14:34 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
-import { nitro } from 'nitro/vite'; // <-- Make sure this plugin is installed and imported
 
 export default defineConfig({
-  plugins: [tanstackStart(), nitro(), viteReact()],
+  plugins: [tanstackStart(), viteReact()],
 });
